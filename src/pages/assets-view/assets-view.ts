b4bfef3ae767898dba63d8e69f6885e8f2e9d4f1
ahaha0807 <b5331@oic.jp>
2018-01-22 21:34:36 +0900

    リストのviewモードの切り替えを実装 #2
    所持コインリストのカードをクリックでJPY表示・枚数表示を切り替える
    同時にセミコロンを削除

--- src/pages/assets-view/assets-view.ts
+++ src/pages/assets-view/assets-view.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { AssetsEditPage } from "../assets-edit/assets-edit";
-import { RestProvider } from "../../providers/rest/rest";
+import { Component } from '@angular/core'
+import { IonicPage, NavController, NavParams } from 'ionic-angular'
+import { AssetsEditPage } from "../assets-edit/assets-edit"
+import { RestProvider } from "../../providers/rest/rest"
 
 /**
  * Generated class for the AssetsViewPage page.
@@ -16,36 +16,36 @@ import { RestProvider } from "../../providers/rest/rest";
     templateUrl: 'assets-view.html',
 })
 export class AssetsViewPage {
-    information: any;
-    total: number;
+    information: any
+    total: number
+    viewMode: string
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private restProvider: RestProvider) {
         this.restProvider.getAssets()
             .then(data => {
-                this.information = data;
+                this.information = data
             }).then(() => {
             this.information = this.information.map(element => {
-                let temp = element.amount * element.price_jpy * 100;
-                element.jpy = Math.round(temp) / 100;
-                return element;
-            });
+                let temp = element.amount * element.price_jpy * 100
+                element.jpy = Math.round(temp) / 100
+                return element
+            })
 
-            this.total = 0;
+            this.total = 0
             this.information.forEach(element => {
-                this.total += element.jpy;
-            });
+                this.total += element.jpy
+            })
             this.total = Math.round(this.total)
-        })
 
+            this.viewMode = 'jpy'
+        })
     }
 
     goAssetsEditPage() {
-        this.navCtrl.setRoot(AssetsEditPage);
+        this.navCtrl.setRoot(AssetsEditPage)
     }
 
-
-    ionViewDidLoad() {
-        console.log('ionViewDidLoad AssetsViewPage');
+    switchAssets() {
+        this.viewMode == 'jpy' ? this.viewMode = 'coin' : this.viewMode = 'jpy'
     }
-
 }
