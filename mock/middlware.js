b7b34444a16bfd79a88d0ce3510b687e2cdf4430
ahaha0807 <b5331@oic.jp>
2018-01-25 02:14:21 +0900

    middlewareの処理のミスを修正
    String#splice()の使い方を間違えていた。

--- mock/middlware.js
+++ mock/middlware.js
@@ -1,9 +1,9 @@
 const jsonServer = require('json-server')
-const router = jsonServer.router('./mock/mock-server.json')
+const router = jsonServer.router('./mock-server.json')
 const db = router.db
 
 module.exports = (req, res, next) => {
-    if (req.method === 'PUT' && req.url.split()[1] === 'assets') {
+    if (req.method === 'PUT' && req.url.split('/')[1] === 'assets') {
         let id = req.body.id
         let mockData = require('./mock-server.json')
         req.body.price_jpy = mockData.rates.find(element => {
