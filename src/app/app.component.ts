d44edeec3afdba126fd9cb32fb99cba7167b849e
ahaha0807 <b5331@oic.jp>
2018-01-29 00:52:01 +0900

    サイドメニューの修正
    不必要なページの削除と表示名の編集

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
 
 import { HomePage } from '../pages/home/home'
-import { ListPage } from '../pages/list/list'
 import { AssetsViewPage } from "../pages/assets-view/assets-view"
 
 @Component({
@@ -23,8 +22,7 @@ export class MyApp {
         // used for an example of ngFor and navigation
         this.pages = [
             {title: 'Home', component: HomePage},
-            {title: 'List', component: ListPage},
-            {title: 'AssetsView', component: AssetsViewPage}
+            {title: '総資産', component: AssetsViewPage}
         ]
 
     }
